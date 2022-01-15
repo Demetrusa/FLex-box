9d55833f57763f32fd04a1da29161f0b3fc1870d
Demetrusa <demetre.kurdadze20b@geolab.edu.gegit config --global user.name Demetrusacd /Desktop/Users/Git/Demegit config --global user.email demetre.kurdadze20b@geolab.edu.ge>
2022-01-15 22:26:43 +0400

    zzz

---
+++ flexi gio/index.js
@@ -0,0 +1,97 @@
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn1");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+let modalTwo = document.getElementById("myModalTwo")
+let btn2 = document.getElementById("myBtn2");
+let span2 = document.getElementsByClassName("closeTwo")[0];
+
+btn2.onclick = function (){
+  modalTwo.style.display = "block";
+}
+span2.onclick = function() {
+  modalTwo.style.display = "none";
+}
+
+let modalThree = document.getElementById("myModalThree")
+let btn3 = document.getElementById("myBtn3");
+let span3 = document.getElementsByClassName("closeThree")[0];
+
+btn3.onclick = function (){
+  modalThree.style.display = "block";
+}
+span3.onclick = function (){
+  modalThree.style.display = "none";
+}
+
+let modalFour = document.getElementById("myModalFour")
+let btn4 = document.getElementById("myBtn4");
+let span4 = document.getElementsByClassName("closeFour")[0];
+
+btn4.onclick = function (){
+  modalFour.style.display = "block";
+}
+span4.onclick = function (){
+  modalFour.style.display = "none";
+}
+
+let modalFive = document.getElementById("myModalFive")
+let btn5 = document.getElementById("myBtn5");
+let span5 = document.getElementsByClassName("closeFive")[0];
+
+btn5.onclick = function (){
+  modalFive.style.display = "block";
+}
+span5.onclick = function (){
+  modalFive.style.display = "none";
+}
+let modalSix = document.getElementById("myModalSix")
+let btn6 = document.getElementById("myBtn6");
+let span6 = document.getElementsByClassName("closeSix")[0];
+
+btn6.onclick = function (){
+  modalSix.style.display = "block";
+}
+span6.onclick = function (){
+  modalSix.style.display = "none";
+}
+
+let modalSeven = document.getElementById("myModalSeven")
+let btn7 = document.getElementById("myBtn7");
+let span7 = document.getElementsByClassName("closeSeven")[0];
+
+btn7.onclick = function (){
+  modalSeven.style.display = "block";
+}
+span7.onclick = function (){
+  modalSeven.style.display = "none";
+}
+let modalEight = document.getElementById("myModalEight")
+let btn8 = document.getElementById("myBtn8");
+let span8 = document.getElementsByClassName("closeEight")[0];
+
+btn8.onclick = function (){
+  modalEight.style.display = "block";
+}
+span8.onclick = function (){
+  modalEight.style.display = "none";
+}
+
+let modalNine = document.getElementById("myModalNine")
+let btn9 = document.getElementById("myBtn9");
+let span9 = document.getElementsByClassName("closeNine")[0];
+
+btn9.onclick = function (){
+  modalNine.style.display = "block";
+}
+span9.onclick = function (){
+  modalNine.style.display = "none";
+}
